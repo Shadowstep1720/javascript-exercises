ccd7d618945916aa7911aea8230c4fa05e45614a
Shadowstep1720 <74839190+Shadowstep1720@users.noreply.github.com>
2024-08-04 22:01:24 -0400

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(message, repetitions) {
+    if (repetitions < 0) {
+        return "ERROR";
+    } 
+    let newMessage = "";
+    for (let i = 0; i < repetitions; i++) {
+        newMessage += message;
+    }
+    return newMessage;
 };
 
 // Do not edit below this line
