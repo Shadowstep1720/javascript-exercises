d225941c5d232c057a24d0b0488cdefd338c4de7
Shadowstep1720 <74839190+Shadowstep1720@users.noreply.github.com>
2024-08-04 22:16:36 -0400

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(message) {
+    let string = '';
+    for (let i = message.length - 1; i >= 0; i--) {
+        string += message[i];
+    }
+    return string;
 };
 
 // Do not edit below this line
