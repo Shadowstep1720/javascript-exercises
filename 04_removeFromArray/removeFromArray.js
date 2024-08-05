76380a987019e68e40806ff4f9946f7d3cd679dd
Shadowstep1720 <74839190+Shadowstep1720@users.noreply.github.com>
2024-08-05 00:07:15 -0400

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removedValues) {
+   let newArray = [];
+    array.forEach(item => {
+        if (!removedValues.includes(item)) {
+            newArray.push(item);
+        }
+    })
+    return newArray;
 };
 
 // Do not edit below this line
